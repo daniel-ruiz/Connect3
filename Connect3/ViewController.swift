4146dde9b1e567a926548382ac0b767d24d387b8
Daniel Ruiz <danruicar@gmail.com>
2017-03-06 21:23:26 +0100

    Initial Commit

---
+++ Connect3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Connect3
+//
+//  Created by Daniel Ruiz on 06/03/2017.
+//  Copyright © 2017 Daniel Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
